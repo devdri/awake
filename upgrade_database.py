796e0a5c7700413be472a3a38146edc320c0187b
qwertyzzz18 <asentman.s6@gmail.com>
2014-01-28 22:20:07 -0800

    Config fixes, fixed broken database-upgrade
    Fixed things broken in previous commits, removed json files from
    roms/.gitignore

--- upgrade_database.py
+++ upgrade_database.py
@@ -7,34 +7,43 @@ parser = argparse.ArgumentParser()
 parser.add_argument('file', nargs='?', default='roms/blue.gb')
 
 def getDB(filename):
-        split=os.path.splitext(filename)
-        if split[1]=='.gb':
-            return split[0]+'.awakedb'
-        if split[1]=='':
-            return split[0]+'.awakedb'
-        elif split[1]=='.awakedb':
-            return filename
-        else:
-            raise ValueError("Not a rom or database!")
+    split=os.path.splitext(filename)
+    if split[1]=='.gb':
+        return split[0]+'.awakedb'
+    if split[1]=='':
+        return split[0]+'.awakedb'
+    elif split[1]=='.awakedb':
+        return filename
+    else:
+        raise ValueError("Not a rom or database!")
 
-def upgradeDatabase(filename):
-    while dbv.detectVersion(filename)!=LATEST_VERSION:
-        dbu.upgrade(filename)
+def upgradeDatabase(connection):
+    ver=dbv.detectVersion(connection)
+    for i in xrange(LATEST_VERSION-ver):
+        dbu.upgrade(connection)
+        connection.commit()
     
+def doUpgrade(name):
+    filename=getDB(name)
+    connection = sqlite3.connect(filename, detect_types=sqlite3.PARSE_DECLTYPES)
+    ver=dbv.detectVersion(connection)
+    print "Database is version",ver
+    print "Latest version is",LATEST_VERSION
+    if ver!=LATEST_VERSION:
+        connection.close()     #Close the connection, make a backup, and re-connect.
+        shutil.copy(filename,filename+".ver"+str(ver)+".bak")
+        connection = sqlite3.connect(filename, detect_types=sqlite3.PARSE_DECLTYPES)
+        upgradeDatabase(connection)
+        print "Done!"
+    else:
+        print "Nothing to do!"
+    connection.commit()
+    connection.close()
     
 if __name__ == '__main__':
     args = parser.parse_args()
     
     if args.file:
-        filename=getDB(args.file)
-        ver=dbv.detectVersion(filename)
-        print "Database is version",ver
-        print "Latest version is",LATEST_VERSION
-        if ver!=LATEST_VERSION:
-            shutil.copy(filename,filename+".ver"+str(ver)+".bak")
-            upgradeDatabase(filename)
-            print "Done!"
-        else:
-            print "Nothing to do!"
+        doUpgrade(args.file)
     else:
         print "No rom file selected."
